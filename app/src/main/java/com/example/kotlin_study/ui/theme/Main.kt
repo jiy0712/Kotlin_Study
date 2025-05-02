a0d63f77dc2d3ce929e27e0ae57959fc7e68c58d
jiy0712 <s2325@e-mirim.hs.kr>
2025-05-02 15:12:53 +0900

    data class

--- app/src/main/java/com/example/kotlin_study/ui/theme/Main.kt
+++ app/src/main/java/com/example/kotlin_study/ui/theme/Main.kt
@@ -2,7 +2,7 @@ package com.example.kotlin_study.ui.theme
 
 fun main() {
    val jiyoung = Person("jiyoung", 19)
-    print(jiyoung.name)
+    print(jiyoung.name) //private으로 getter도 불가 (에러)
     print(jiyoung.age)
 
     jiyoung.age = 23 //var로 수정 가능
@@ -12,7 +12,7 @@ fun main() {
 //코틀린의 기본 생성자를 괄호 안에 작성
 //Alt + Enter로 put ~ 를 이용해 깔끔히 정리
 class Person(
-    val name: String,
+    private val name: String, //getter, setter 모두 불가 (기본값은 public)
     var age: Int
 ){
 
