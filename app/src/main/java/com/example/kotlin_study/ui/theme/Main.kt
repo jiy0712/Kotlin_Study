8af06acfc175a4940b947fbc1b12ba0db2f32922
jiy0712 <s2325@e-mirim.hs.kr>
2025-05-02 15:23:55 +0900

    data class (getter, setter)

--- app/src/main/java/com/example/kotlin_study/ui/theme/Main.kt
+++ app/src/main/java/com/example/kotlin_study/ui/theme/Main.kt
@@ -1,19 +1,28 @@
 package com.example.kotlin_study.ui.theme
 
 fun main() {
-   val jiyoung = Person("jiyoung", 19)
-    print(jiyoung.name) //private으로 getter도 불가 (에러)
-    print(jiyoung.age)
+    val jiyoung = Person("jiyoung", 19)
+    val jiyoung2 = Person("jiyoung", 19)
 
-    jiyoung.age = 23 //var로 수정 가능
-    //class의 val와 var를 이용해 getter, setter 기능 가능
+    println(jiyoung) // 해시코드 : @6b143ee9
+    println(jiyoung2) // 해시코드 : @1936f0f5
+    print(jiyoung == jiyoung2) //F로 나옴 (다른사람이니)
+    jiyoung.hobby = "sing" //여기서도 바꿀 수 있다
 }
-
-//코틀린의 기본 생성자를 괄호 안에 작성
-//Alt + Enter로 put ~ 를 이용해 깔끔히 정리
-class Person(
-    private val name: String, //getter, setter 모두 불가 (기본값은 public)
+//data : 재정의 (data class). 이름과 나이가 같으니 같은 사람으로 값이 나온다
+data class Person(
+    private val name: String,
     var age: Int
 ){
+    var hobby = "축구"
 
+    //더 무언가를 하고 싶다면 (실행할때마다 init안의 코드가 실행된다)
+    init{
+        println("init");
+    }
+
+    //생성자안에 별도의 프롬프트가 있다면
+    fun some(){
+        hobby = "농구"
+    }
 }
