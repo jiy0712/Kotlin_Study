e258d5d86035ce3f5ed0982dc1f5d90679126a62
jiy0712 <s2325@e-mirim.hs.kr>
2025-05-02 15:45:21 +0900

    callBack함수 (람다식)

--- app/src/main/java/com/example/kotlin_study/ui/theme/Main.kt
+++ app/src/main/java/com/example/kotlin_study/ui/theme/Main.kt
@@ -1,12 +1,16 @@
 package com.example.kotlin_study.ui.theme
 
+// 람다식
 fun main() {
-    val box = Box(10)
-    val box2 = Box("dfdfd")
-
-    print(box.value)
+    myFunc(10) { //전달하는게 하나라면 myFunc(){}에서 ()생략 가능
+        println("함수 호출")
+    }
 }
-//제네릭(프로그래밍에서 타입 파라미터를 사용하여 코드를 일반화하고 재사용성을 높이는 방법)
-class Box<T>(var value: T){
+//함수는 input, output 정의하기
 
+//input, output 아무것도 없다면
+fun myFunc(a: Int, callBack : () -> Unit){
+    println("함수시작")
+    callBack() //함수호출
+    println("함수끝")
 }
