c495ce4a9d3f30f89ff911aa01cb6d82aed5b648
jiy0712 <s2325@e-mirim.hs.kr>
2025-05-02 15:11:40 +0900

    class

--- app/src/main/java/com/example/kotlin_study/ui/theme/Main.kt
+++ app/src/main/java/com/example/kotlin_study/ui/theme/Main.kt
@@ -1,18 +1,19 @@
 package com.example.kotlin_study.ui.theme
 
 fun main() {
-    print(sum(10, 20))
-    print(sum(b = 10, a = 20)) //a,b 순서 안맞춰도 에러 안남
+   val jiyoung = Person("jiyoung", 19)
+    print(jiyoung.name)
+    print(jiyoung.age)
+
+    jiyoung.age = 23 //var로 수정 가능
+    //class의 val와 var를 이용해 getter, setter 기능 가능
 }
 
-//int a, b의 합을 int로 반환하는 함수
-fun sum(a: Int, b: Int) : Int{
-    return a + b
+//코틀린의 기본 생성자를 괄호 안에 작성
+//Alt + Enter로 put ~ 를 이용해 깔끔히 정리
+class Person(
+    val name: String,
+    var age: Int
+){
+
 }
-
-//코틀린 내용이 한줄이라면
-//fun sum(a: Int, b: Int) : Int = a + b 로 가능
-////fun sum(a: Int, b: Int) = a + b 로 가능 (리턴 타입 생략 가능)
-
-fun sum(a: Int, b: Int, c: Int = 0) = a + b + c
-//c에 디폴트값이 있다면 print(sum(10, 20)) 해도 가능
