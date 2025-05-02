3936bef2a17508611d0f8812c44a9cc96224b137
jiy0712 <s2325@e-mirim.hs.kr>
2025-05-02 15:26:20 +0900

    getter, setter 재정의(제어)

--- app/src/main/java/com/example/kotlin_study/ui/theme/Main.kt
+++ app/src/main/java/com/example/kotlin_study/ui/theme/Main.kt
@@ -7,7 +7,6 @@ fun main() {
     println(jiyoung) // 해시코드 : @6b143ee9
     println(jiyoung2) // 해시코드 : @1936f0f5
     print(jiyoung == jiyoung2) //F로 나옴 (다른사람이니)
-    jiyoung.hobby = "sing" //여기서도 바꿀 수 있다
 }
 //data : 재정의 (data class). 이름과 나이가 같으니 같은 사람으로 값이 나온다
 data class Person(
@@ -15,6 +14,8 @@ data class Person(
     var age: Int
 ){
     var hobby = "축구"
+        private set //이렇게 붙이면 외부(fun main)에서 수정 못함
+        get() = "취미 : $field" //getter 재정의
 
     //더 무언가를 하고 싶다면 (실행할때마다 init안의 코드가 실행된다)
     init{
