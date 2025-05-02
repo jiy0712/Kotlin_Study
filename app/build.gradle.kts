2a26d3fd61e15aa7ad2cc2c14f1700100fd1f112
jiy0712 <s2325@e-mirim.hs.kr>
2025-05-02 15:54:09 +0900

    코루틴

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- app/src/main/java/com/example/kotlin_study/ui/theme/Main.kt
+++ app/src/main/java/com/example/kotlin_study/ui/theme/Main.kt
@@ -2,15 +2,15 @@ package com.example.kotlin_study.ui.theme
 
 // 람다식
 fun main() {
-    myFunc(10) { //전달하는게 하나라면 myFunc(){}에서 ()생략 가능
-        println("함수 호출")
+    lifecycleScope.launch{
+        myFunc(10) {
+            println("함수 호출")
+        }
     }
 }
-//함수는 input, output 정의하기
 
-//input, output 아무것도 없다면
-fun myFunc(a: Int, callBack : () -> Unit){
+suspend fun myFunce(a: Int, callBack: () -> Unit = {}){
     println("함수시작")
-    callBack() //함수호출
+    callBack()
     println("함수끝")
 }
